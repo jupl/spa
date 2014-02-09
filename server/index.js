e11b0b622ac0b7c8a39154e776451d489a26dab5
Juan Placencia <juan.placencia.512@gmail.com>
2014-02-08 20:03:24 -0600

    Fix logger

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ var setupSessions = require('./sessions');
 var app = module.exports = express();
 
 // Add middleware
-if(process.env.NODE_ENV === 'development') {
+if(process.env.NODE_ENV !== 'production') {
   app.use(express.logger('dev'));
 }
 app.use(express.compress());
