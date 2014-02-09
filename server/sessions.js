aab95ec95277b07316a4dfc45110cb91afc65cd2
Juan Placencia <juan.placencia.512@gmail.com>
2014-02-08 18:19:01 -0600

    Tweak

--- server/sessions.js
+++ server/sessions.js
@@ -4,7 +4,7 @@ var MongoStore = require('connect-mongo')(express);
 module.exports = function(app) {
   // Set up sessions with MongoDB
   app.use(express.session({
-    secret: 'SECRET PLACEHOLDER',
+    secret: 'PLACEHOLDER',
     store: new MongoStore({db: 'sessions'})
   }));
 };
