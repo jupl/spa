7bdacd5d8d10f96daac6ca0f3189e7dda83989ab
jupl <juan.placencia.512@gmail.com>
2014-09-28 21:41:58 -0500

    Add at-2x support for retina images

--- config/gulp/build/styles.js
+++ config/gulp/build/styles.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var at2x = require('rework-plugin-at2x');
 var csso = require('gulp-csso');
 var gulp = require('gulp');
 var myth = require('gulp-myth');
 var path = require('path');
 var plumber = require('gulp-plumber');
+var rework = require('gulp-rework');
 var util = require('gulp-util');
 var config = require('../../../config');
 
@@ -13,6 +15,7 @@ gulp.task('build:styles', function() {
   .src(config.globs.styles.build)
   .pipe((config.watch ? plumber : util.noop)())
   .pipe(myth({sourcemap: config.environment.development}))
+  .pipe(rework(at2x(), {sourcemap: config.environment.development}))
   .pipe((config.environment.production ? csso : util.noop)())
   .pipe(gulp.dest(config.paths.destination.styles));
 });
